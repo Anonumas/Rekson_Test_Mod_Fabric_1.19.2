e10a3522b04538c67abd59aa97320004058422bb
anonumas <doe607037@gmail.com>
2023-08-12 23:37:50 -0400

    Rekson Statue Now Cycles through Each Rekson Quote and Individual Statues Can Be at Different Stages in Cycle

--- src/main/java/net/anonumas/reksontestmod/block/custom/ReksonStatueBlock.java
+++ src/main/java/net/anonumas/reksontestmod/block/custom/ReksonStatueBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
@@ -13,7 +14,6 @@ import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
@@ -21,8 +21,10 @@ import org.jetbrains.annotations.Nullable;
 
 public class ReksonStatueBlock extends HorizontalFacingBlock {
     public ReksonStatueBlock(Settings settings) {
-        super(settings);
+        super(settings); setDefaultState(getDefaultState().with(soundNumber,0));
     }
+    public static IntProperty soundNumber = IntProperty.of("soundnumber",0,9);
+
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
 
 
@@ -63,15 +65,62 @@ public class ReksonStatueBlock extends HorizontalFacingBlock {
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, soundNumber);
     }
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (world.isClient && hand == Hand.MAIN_HAND){
-            outputRandomSound(player);
+            outputRandomSound(player, world.getBlockState(pos), world, pos);
+            return ActionResult.SUCCESS;
         }
+        state = (BlockState)state.cycle(soundNumber);
+        world.setBlockState(pos, state, Block.NOTIFY_ALL);
+
         return super.onUse(state, world, pos, player, hand, hit);
     }
 
+
+//Needs Blockstate state to function
+    private void outputRandomSound(PlayerEntity player, BlockState state, World world, BlockPos pos){
+
+        switch (world.getBlockState(pos).get(soundNumber)) {
+            case 0 -> playSoundandSetState(player,0);
+            case 1 -> playSoundandSetState(player,1);
+            case 2 -> playSoundandSetState(player,2);
+            case 3 -> playSoundandSetState(player,3);
+            case 4 -> playSoundandSetState(player,4);
+            case 5 -> playSoundandSetState(player,5);
+            case 6 -> playSoundandSetState(player,6);
+            case 7 -> playSoundandSetState(player,7);
+            case 8 -> playSoundandSetState(player,8);
+            case 9 -> playSoundandSetState(player,9);
+        }
+    }
+    public static void playSoundandSetState(PlayerEntity player, int x){
+        if (x == 0){
+            player.playSound(ModSounds.REKGASM, 1f, 1f);
+        } else if (x == 1) {
+            player.playSound(ModSounds.EREKSON_DYSFUNCTION, 1f, 1f);
+        } else if (x == 2) {
+            player.playSound(ModSounds.REKSON_ALWAYS_LICK, 1f, 1f);
+        } else if (x == 3) {
+            player.playSound(ModSounds.REKSON_APPRECIATES_TIPS, 1f, 1f);
+        } else if (x == 4) {
+            player.playSound(ModSounds.REKSON_HUMPS, 1f, 1f);
+        } else if (x == 5) {
+            player.playSound(ModSounds.REKSON_UNFAIR, 1f, 1f);
+        } else if (x == 6) {
+            player.playSound(ModSounds.REKSON_VIBRATING_SWORD, 1f, 1f);
+        } else if (x == 7) {
+            player.playSound(ModSounds.REKSON_HAGGLES_FOR_HEAD, 1f, 1f);
+        } else if (x == 8) {
+            player.playSound(ModSounds.REKSON_CANT_GET_IT_OUT, 1f, 1f);
+        } else if (x == 9) {
+            player.playSound(ModSounds.REKSON_SHOVE_IT, 1f, 1f);
+            }
+
+    }
+    //Random functionality for outputRandomSound Import Minecraft's Random to Use
+    /*
     private void outputRandomSound(PlayerEntity player){
 
         switch (getRandomNumber()) {
@@ -92,5 +141,5 @@ public class ReksonStatueBlock extends HorizontalFacingBlock {
     private int getRandomNumber(){
         return Random.createLocal().nextInt(10);
     }
-
+*/
 }
